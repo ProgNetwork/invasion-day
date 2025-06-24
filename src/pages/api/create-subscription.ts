264e57c46b4d19f09c85606c4aa221c2796513d2
benmort <benjaminmort@gmail.com>
2025-06-24 21:09:28 +1000

    changed stripe api version to named

--- src/pages/api/create-subscription.ts
+++ src/pages/api/create-subscription.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: 'latest',
+  apiVersion: '2025-05-28.basil',
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {

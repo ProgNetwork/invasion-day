a07bc3714a0bfbfa2f7abf6c036dc8e41c681e4e
benmort <benjaminmort@gmail.com>
2025-12-18 11:43:49 +1000

    and allies for summit

--- src/pages/summit-2026.tsx
+++ src/pages/summit-2026.tsx
@@ -25,14 +25,14 @@ const Summit2026 = () => {
             <h2 className="text-3xl font-bold tracking-tight text-primary-700 sm:text-4xl font-rock-salt"><span className="block">Common Threads <br />Summit 2026</span></h2>
             <br/>
             <p className="text-md mb-8 leading-relaxed text-gray-900 sm:text-2xl font-bold">
-              A three-day summit designed for, and by, First Nations people - to connect, yarn, learn and plan for action
+              A three-day summit designed for, both First Nations people and allies - to connect, yarn, learn and plan for action
             </p>
 
             <p className="text-2xl font-bold text-orange-600 mb-2">WHERE: <span className="text-gray-900">Naarm / Melbourne (exact location to be shared closer to the date)</span></p>
             <p className="text-2xl font-bold text-orange-600 mb-8">WHEN: <span className="text-gray-900">Saturday 21 February - Sunday 22 February 2026 (~10am-4pm each day)</span></p>
 
             <p className="text-lg text-gray-700 mb-4">
-              Who: First Nations people and allies from across the continent who are keen to grow the Together for Treaty campaign in your community
+              Who: Both First Nations people and allies from across the continent who are keen to grow the Together for Treaty campaign in your community
             </p>
             <p className="text-lg text-gray-700 mb-8">
               If you have any questions, please get in touch with us via <a href="mailto:info@commonthreads.org.au" className="text-blue-600 hover:underline">info@commonthreads.org.au</a> (please note any enquiries during the holiday season will be responded to in January).
